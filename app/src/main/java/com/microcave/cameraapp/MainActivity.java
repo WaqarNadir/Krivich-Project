74d00014c109d8bc21e966a09abc770a2918e632
Waqar Nadir <m_vicky71@yahoo.com>
2015-07-07 18:38:56 +0500

    Take pic on screen off

--- app/src/main/java/com/microcave/cameraapp/MainActivity.java
+++ app/src/main/java/com/microcave/cameraapp/MainActivity.java
@@ -1,25 +1,23 @@
 package com.microcave.cameraapp;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
+import android.content.ServiceConnection;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -27,7 +25,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -39,6 +36,8 @@ public class MainActivity extends ActionBarActivity {
     OrientationEventListener listner;
     public Context c;
 
+    public boolean Serviceresult;
+
     SendImage SI= new SendImage();
 
 String Url= "https://image-judger.herokuapp.com/api/images";
@@ -46,10 +45,17 @@ String Url= "https://image-judger.herokuapp.com/api/images";
     ArrayList<String> ImagePath=new ArrayList<String>();
     int count =10;
     ImageView i;
+    Intent is;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         c=this;
         super.onCreate(savedInstanceState);
+Log.e("activity ", "started");
+        Serviceresult=true;
+        is  = new Intent(this,backgroundservice.class);
+
+
         setContentView(R.layout.activity_main);
          i= (ImageView)findViewById(R.id.imageView);
 
@@ -75,47 +81,135 @@ String Url= "https://image-judger.herokuapp.com/api/images";
                 Delete();
             }
         });
+        listner= new OrientationEventListener(c) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+
+                if(mCamera==null)
+                {
+                    mCamera=Camera.open();
+                }
+
+                if(orientation==180) {
+                   mCameraView.rotation_In_progress=true;
+                    Log.e("Camera Status", "180 called " + mCameraView.rotation_In_progress);
+                }
+                if(orientation==90) {
+                    mCameraView.rotation_In_progress=true;
+                    Log.e("Camera Status", "90 called " + mCameraView.rotation_In_progress);
+                }
+                if(orientation==270) {
+                    mCameraView.rotation_In_progress=true;
+                    Log.e("Camera Status", "270 called " + mCameraView.rotation_In_progress);
+                }
+            }
+        };
+
+        listner.enable();
 
 
     }
+
     @Override
     protected void onPause() {
         super.onPause();
 
         Log.e("Camera Status", "Pause called");
-        mCamera.release();
+       mCamera.release();
+
 
         mCameraView.getHolder().removeCallback(mCameraView);
 
+        s.interrupt();
+
+        this.bindService(is, mServerConn, BIND_AUTO_CREATE);
+        this.startService(is);
+        isconnected=true;
+        Serviceresult=false;
+        run_thread=false;
+finish();
+
     }
+    protected ServiceConnection mServerConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            Log.d("service ", "onServiceConnected");
+
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d("service ", "onServiceDisconnected");
+        }
+    };
+Boolean isconnected=false;
+    Thread s= new cameraintializer();
+    Boolean run_thread=true;
+
 
     @Override
     protected void onResume() {
         super.onResume();
+
         Log.e("Camera Status", "resume called");
-        if(mCamera==null)
-        {
-            mCamera=Camera.open();
+if(isconnected) {
+    mCamera=null;
+    Log.e("is connected ", "true");
+    this.stopService(new Intent(this, backgroundservice.class));
+    this.unbindService(mServerConn);
+    Serviceresult=false;        //-----
+    try {
+        Thread thread= new cameraintializer();
+        thread.start();
+        synchronized (thread) {
+            thread.wait(1000);
         }
-        mCameraView.getHolder().addCallback(mCameraView);
-       // mCameraView.change(mCamera);
 
-        timer = new Timer();
-        ClickPhoto= new TakePhoto();
-        ChangeImage= new ChangePhoto();
-        t=new Timer();
-        count=10;
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    this.finish();
 
-        timer.schedule(ChangeImage, 0, 1000);//Update text every second
+
+}
+
+
+if(Serviceresult) {
+    Log.e("service result", "true");
+        run_thread=true;
+    s.start();
+}
+
     }
 
+    private class cameraintializer extends Thread
+    {
+        @Override
+        public void run() {
+            if (run_thread) {
+                if (mCamera == null) {
+                    mCamera = Camera.open();
+                }
+                mCameraView.getHolder().addCallback(mCameraView);
+                // mCameraView.change(mCamera);
 
+                timer = new Timer();
+                ClickPhoto = new TakePhoto();
+                ChangeImage = new ChangePhoto();
+                t = new Timer();
+                count = 10;
+
+                timer.schedule(ChangeImage, 0, 1000);//Update text every second
+            }
+        }
+    }
     public Timer timer = new Timer();
 
     public   Timer t=new Timer();
     public  TimerTask ClickPhoto= new TakePhoto();
-
-    private  class ChangePhoto extends  TimerTask
+    ChangePhoto ChangeImage= new ChangePhoto();
+    public   class ChangePhoto extends  TimerTask
     {
 
         @Override
@@ -183,17 +277,23 @@ String Url= "https://image-judger.herokuapp.com/api/images";
         }
     }
 
-    private  class TakePhoto extends  TimerTask
+    public   class TakePhoto extends  TimerTask
     {
+
 
         @Override
         public void run() {
-
+        if(!mCameraView.rotation_In_progress) {
+            if(Serviceresult)                   //checks activity is on top or not
+            {
             mCamera.takePicture(null, null, jpegCallback);
+            }
+        }
             if(count==5) {
                 t.cancel();
                 count=10;
                 Log.e("update display", "");
+
 
                 timer= new Timer();
                 ChangeImage= new ChangePhoto();
@@ -204,7 +304,7 @@ String Url= "https://image-judger.herokuapp.com/api/images";
         }
     }
 
-    ChangePhoto ChangeImage= new ChangePhoto();
+
 
     public void updateDisplay() {
         timer= new Timer();
@@ -231,7 +331,7 @@ String Url= "https://image-judger.herokuapp.com/api/images";
         mCamera.startPreview();
     }
 
-    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+    public class SaveImageTask extends AsyncTask<byte[], Void, Void> {
 
         @Override
         protected Void doInBackground(byte[]... data) {
@@ -274,7 +374,7 @@ String Url= "https://image-judger.herokuapp.com/api/images";
         }
 
     }
-    private void refreshGallery(File file) {
+    public void refreshGallery(File file) {
         Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         mediaScanIntent.setData(Uri.fromFile(file));
         sendBroadcast(mediaScanIntent);
@@ -301,5 +401,14 @@ String Url= "https://image-judger.herokuapp.com/api/images";
     }
 
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.unbindService(mServerConn);
+        this.stopService(is);
+        Log.e("Main activty", "Destroy called;");
+    }
+
 
 }
+
